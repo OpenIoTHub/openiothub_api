c9ebeaaadb5428da466de2b7fe3fb398909f0364
Yu Fang <yu@iotserv.com>
2021-04-17 23:22:16 +0800

    update

--- lib/api/IoTManager/UserManager.dart
+++ lib/api/IoTManager/UserManager.dart
@@ -1,7 +1,9 @@
+import 'package:grpc/grpc.dart';
 import 'package:iot_manager_grpc_api/pb/common.pb.dart';
 import 'package:iot_manager_grpc_api/pb/userManager.pb.dart';
 import 'package:iot_manager_grpc_api/pb/userManager.pbgrpc.dart';
 import 'package:openiothub_api/api/IoTManager/IoTManagerChannel.dart';
+import 'package:openiothub_api/utils/utils.dart';
 
 class UserManager {
 //  注册用户
@@ -32,8 +34,10 @@ class UserManager {
 
 //  rpc GetUserInfo (Empty) returns (UserInfo) {}
   static Future<UserInfo> GetUserInfo() async {
+    String jwt = await getJWT();
     final channel = await Channel.getDefaultIoTManagerChannel();
-    final stub = UserManagerClient(channel);
+    final stub = UserManagerClient(channel,
+        options: CallOptions(metadata: {'jwt': jwt}));
     Empty empty = Empty();
     UserInfo userInfo = await stub.getUserInfo(empty);
     print('UserInfo: ${userInfo}');
@@ -45,8 +49,10 @@ class UserManager {
 //  rpc UpdateUserNanme (StringValue) returns (OperationResponse) {}
   static Future<OperationResponse> UpdateUserNanme(
       StringValue stringValue) async {
+    String jwt = await getJWT();
     final channel = await Channel.getDefaultIoTManagerChannel();
-    final stub = UserManagerClient(channel);
+    final stub = UserManagerClient(channel,
+        options: CallOptions(metadata: {'jwt': jwt}));
     OperationResponse operationResponse =
         await stub.updateUserNanme(stringValue);
     print('UpdateUserNanme: ${operationResponse}');
@@ -57,8 +63,10 @@ class UserManager {
 //  rpc UpdateUserEmail (StringValue) returns (OperationResponse) {}
   static Future<OperationResponse> UpdateUserEmail(
       StringValue stringValue) async {
+    String jwt = await getJWT();
     final channel = await Channel.getDefaultIoTManagerChannel();
-    final stub = UserManagerClient(channel);
+    final stub = UserManagerClient(channel,
+        options: CallOptions(metadata: {'jwt': jwt}));
     OperationResponse operationResponse =
         await stub.updateUserEmail(stringValue);
     print('UpdateUserEmail: ${operationResponse}');
@@ -69,8 +77,10 @@ class UserManager {
 //  rpc UpdateUserMobile (StringValue) returns (OperationResponse) {}
   static Future<OperationResponse> UpdateUserMobile(
       StringValue stringValue) async {
+    String jwt = await getJWT();
     final channel = await Channel.getDefaultIoTManagerChannel();
-    final stub = UserManagerClient(channel);
+    final stub = UserManagerClient(channel,
+        options: CallOptions(metadata: {'jwt': jwt}));
     OperationResponse operationResponse =
         await stub.updateUserMobile(stringValue);
     print('UpdateUserMobile: ${operationResponse}');
@@ -81,8 +91,10 @@ class UserManager {
 //  rpc UpdateUserPassword (StringValue) returns (OperationResponse) {}
   static Future<OperationResponse> UpdateUserPassword(
       StringValue stringValue) async {
+    String jwt = await getJWT();
     final channel = await Channel.getDefaultIoTManagerChannel();
-    final stub = UserManagerClient(channel);
+    final stub = UserManagerClient(channel,
+        options: CallOptions(metadata: {'jwt': jwt}));
     OperationResponse operationResponse =
         await stub.updateUserPassword(stringValue);
     print('UpdateUserPassword: ${operationResponse}');
@@ -93,8 +105,10 @@ class UserManager {
 //  rpc UpdateUserAvatar (UpdateAvatar) returns (OperationResponse) {}
   static Future<OperationResponse> UpdateUserAvatar(
       UpdateAvatar updateAvatar) async {
+    String jwt = await getJWT();
     final channel = await Channel.getDefaultIoTManagerChannel();
-    final stub = UserManagerClient(channel);
+    final stub = UserManagerClient(channel,
+        options: CallOptions(metadata: {'jwt': jwt}));
     OperationResponse operationResponse =
         await stub.updateUserAvatar(updateAvatar);
     print('OperationResponse: ${operationResponse}');
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,6 +1,13 @@
+import 'package:openiothub_constants/openiothub_constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uuid/uuid.dart';
 
 String getOneUUID() {
   var uuid = new Uuid();
   return uuid.v4();
+}
+
+Future<String> getJWT() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getString(SharedPreferencesKey.USER_TOKEN_KEY);
 }

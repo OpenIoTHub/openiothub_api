a742ea71df6d47ea38cbfcf01a0dcc5f08cdb716
Yu Fang <yu@iotserv.com>
2021-04-19 21:20:36 +0800

    第三方zip设备的支持

--- lib/openiothub_api.dart
+++ lib/openiothub_api.dart
@@ -1,7 +1,5 @@
 library openiothub_api;
 
-export './utils/utils.dart';
-
 export './api/GateWay/GatewayLoginManager.dart';
 
 export './api/IoTManager/UserManager.dart';
@@ -11,3 +9,6 @@ export './api/OpenIoTHub/SessionApi.dart';
 export './api/OpenIoTHub/Utils.dart';
 
 export './api/Server/HttpManager.dart';
+
+export './utils/utils.dart';
+export './utils/zipDevice.dart';

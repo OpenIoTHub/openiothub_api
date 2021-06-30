8cd744d901bdf0e822b749d604cffad638e977f3
Yu Fang <yu@iotserv.com>
2021-06-30 17:29:01 +0800

    update

--- lib/api/GateWay/GatewayLoginManager.dart
+++ lib/api/GateWay/GatewayLoginManager.dart
@@ -4,24 +4,27 @@ import 'package:gateway_grpc_api/pb/service.pbgrpc.dart';
 import 'package:grpc/grpc.dart';
 
 class GatewayLoginManager {
-  static Future<LoginResponse> LoginServerByServerInfo(
-      ServerInfo serverInfo, String gatewayIp, int gatewayPort) async {
-    final channel = await Channel.getGateWayChannel(gatewayIp, gatewayPort);
-    final stub = GatewayLoginManagerClient(channel);
-    LoginResponse loginResponse =
-        await stub.loginServerByServerInfo(serverInfo);
-    print('LoginServerByServerInfo: ${loginResponse}');
-    channel.shutdown();
-    return loginResponse;
-  }
-
-  static Future<Token> GetOpenIoTHubToken(
+  static Future<LoginResponse> CheckGatewayLoginStatus(
       String gatewayIp, int gatewayPort) async {
     final channel = await Channel.getGateWayChannel(gatewayIp, gatewayPort);
     final stub = GatewayLoginManagerClient(channel);
-    Token token = await stub.getOpenIoTHubToken(Empty());
-    print('GetOpenIoTHubToken: ${token}');
+    LoginResponse loginResponse = await stub.checkGatewayLoginStatus(Empty());
+    print('GetOpenIoTHubToken: ${loginResponse}');
     channel.shutdown();
-    return token;
+    //未登录返回false，登录了返回true
+    return loginResponse;
+  }
+
+  static Future<LoginResponse> LoginServerByToken(
+      String tokenValue, String gatewayIp, int gatewayPort) async {
+    final channel = await Channel.getGateWayChannel(gatewayIp, gatewayPort);
+    final stub = GatewayLoginManagerClient(channel);
+    Token token = Token();
+    token.value = tokenValue;
+    LoginResponse loginResponse =
+        await stub.loginServerByToken(token);
+    print('LoginServerByServerInfo: ${loginResponse}');
+    channel.shutdown();
+    return loginResponse;
   }
 }

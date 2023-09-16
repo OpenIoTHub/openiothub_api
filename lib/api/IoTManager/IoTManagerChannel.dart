c16f03d1ae2dad3628b14d116739ec0aaf641df5
iotserv <newfarry@126.com>
2023-09-16 23:11:40 +0800

    update

--- lib/api/IoTManager/IoTManagerChannel.dart
+++ lib/api/IoTManager/IoTManagerChannel.dart
@@ -6,12 +6,6 @@ import 'package:grpc/grpc.dart';
 class Channel {
   static Future<ClientChannel> getDefaultIoTManagerChannel() async {
     ClientChannel channel;
-    if (Platform.isMacOS) {
-      final channel = ClientChannel("127.0.0.1",port: Config.iotManagerRpcPort,
-          options: const ChannelOptions(
-              credentials: const ChannelCredentials.insecure()));
-      return channel;
-    }
     channel = ClientChannel(Config.iotManagergRpcIp,port: Config.iotManagerRpcPort,
         options: const ChannelOptions(
             credentials: const ChannelCredentials.insecure()));
